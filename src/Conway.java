bdb111587f06aa6d0619b9c190f8d90229179dae
Austin <awtotty@gmail.com>
2018-04-29 11:47:33 -0700

    Finish logic, add gui

--- src/Conway.java
+++ src/Conway.java
@@ -1,58 +1,132 @@
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 import java.util.List;
 
 
 public class Conway {
-    HashMap<Point, Boolean> world = new HashMap<Point, Boolean>();
-    Set<Point> keys;
-    Set<Boolean> vals;
-    int generation = 1 ;
-    int rows;
-    int cols;
+    private int generation = 1 ;
+    private final int HASH_SIZE = (int) Math.pow(2, 14);
+    private HashSet<Cell> world = new HashSet<>(HASH_SIZE);
+    private HashMap<Cell, Integer> counts = new HashMap<>(HASH_SIZE);
 
-    // Creates a random 20x20 world
-    public Conway() {
-        for (int i = 0; i < 20; i++) {
-
-
-        }
-    }
-
-
-    public void advanceGeneration() {
-        generation++;
-
-        // Make a new world to build
-        HashMap<Point, Boolean> nextWorld = new HashMap<Point, Boolean>();
-
-        // Handle live cells (rules 1-3)
-        for (Point p : keys) {
-            int n = getNumNeighbors(p);
-            if (n == 2 || n == 3) {
-                nextWorld.put(p, true);
-            }
-        }
-
-        // Handle dead cells (rule 4)
-    }
-
-    public int getNumNeighbors(Point p) {
-        int px = (int) p.getX();
-        int py = (int) p.getY();
-        int neighbors = 0;
-
-        for (int x = px-1; x <= px+1; x++) {
-            for (int y = py-1; y <= py+1; y++) {
-                // neighbor exists and is not the actual point
-                if (world.containsKey(new Point(x,y)) && (px != x && py != y)) {
-                   neighbors++;
+    // Creates a random world
+    public Conway(int size) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                Cell c = new Cell(x, y);
+                // randomly decide if the given cell is alive
+                if ( Math.random() > 0.97 ) {
+                    addCell(c);
                 }
             }
         }
-
-        return neighbors;
     }
+
+    public Conway(List<List<Integer>> initState) {
+        for (List<Integer> cell : initState) {
+            Cell c = new Cell(cell.get(0), cell.get(1));
+            addCell(c);
+        }
+    }
+
+    private void addCell(Cell c){
+        world.add(c);
+        // inc neighbors count
+        for (Cell neighbor : c.getNeighbors()) {
+            if (counts.containsKey(neighbor)) {
+                counts.put(neighbor, counts.get(neighbor) + 1);
+            }
+            else {
+                counts.put(neighbor, 1);
+            }
+        }
+    }
+
+    private void removeCell(Cell c) {
+        world.remove(c);
+        // dec neighbors count
+        for (Cell neighbor : c.getNeighbors()) {
+            if (counts.containsKey(neighbor) && counts.get(neighbor)-1 > 0) {
+                counts.put(neighbor, counts.get(neighbor) - 1);
+            }
+            else counts.remove(neighbor);
+        }
+    }
+
+    public void step() {
+        generation++;
+
+        List<Cell> toRemove = new ArrayList<>();
+        List<Cell> toAdd = new ArrayList<>();
+
+        // Handle live cells (rules 1-3)
+        for (Cell c : world) {
+            Integer n = counts.get(c);
+            if (n == null || n < 2 || n > 3)
+                toRemove.add(c);
+        }
+        // Handle dead cells (rule 4)
+        for (Cell c : counts.keySet()) {
+            Integer n = counts.get(c);
+            if (n == 3 && !world.contains(c))
+                toAdd.add(c);
+        }
+        // Remove all cells from toRemove
+        for (Cell c : toRemove) {
+            removeCell(c);
+        }
+        // Add all cells from toAdd
+        for (Cell c : toAdd) {
+            addCell(c);
+        }
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public boolean isAlive(int x, int y) {
+        return world.contains(new Cell(x,y));
+    }
+
+    private class Cell {
+        int x, y;
+
+        Cell(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other instanceof Cell) {
+                Cell o = (Cell) other;
+                return o.x == x && o.y == y;
+            }
+            else return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return x*3 + y*5;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ')';
+        }
+
+        public Cell[] getNeighbors() {
+            return new Cell[]   {
+                                new Cell(x - 1, y - 1),
+                                new Cell(x - 1, y),
+                                new Cell(x - 1, y + 1),
+                                new Cell(x, y - 1),
+                                new Cell(x, y + 1),
+                                new Cell(x + 1, y - 1),
+                                new Cell(x + 1, y),
+                                new Cell(x + 1, y + 1),
+                                };
+        }
+    }
+
 }
---
+++ src/ConwayGUI.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ConwayGUI extends JComponent {
+    private static int boardSize = 100;
+    private static final int CELL_DIM = 5;
+    private static final int WINDOW_SIZE = boardSize *CELL_DIM;
+    private static final int FPS = 500;
+
+    private static Conway game = new Conway(boardSize);
+
+    public ConwayGUI() {
+        setPreferredSize(new Dimension(WINDOW_SIZE, WINDOW_SIZE));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int x = 0; x < boardSize; x++)
+            for (int y = 0; y < boardSize; y++)
+                if (game.isAlive(x,y))
+                    g.fillRect(x*CELL_DIM, y*CELL_DIM, CELL_DIM, CELL_DIM);
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Game of Life");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add content to the window.
+        // Gen counter
+        JPanel genCounter = new JPanel();
+        genCounter.add(new JLabel("Gen: " + game.getGeneration()));
+        genCounter.setBackground(Color.GRAY);
+        genCounter.setOpaque(true);
+        genCounter.setBounds(0,0, 55,28);
+
+        // Grid
+        JPanel grid = new JPanel();
+        grid.add(new ConwayGUI());
+        grid.setOpaque(true);
+        grid.setBounds(0,0, WINDOW_SIZE, WINDOW_SIZE);
+
+        // Layered panel
+        JLayeredPane lpane = new JLayeredPane();
+        lpane.setBounds(0,0, WINDOW_SIZE, WINDOW_SIZE);
+        lpane.setPreferredSize(new Dimension(WINDOW_SIZE, WINDOW_SIZE));
+        lpane.add(grid, 0, 0);
+        lpane.add(genCounter, 1, 0);
+
+        frame.add(lpane, BorderLayout.CENTER);
+
+
+        Timer timer = new Timer(FPS, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // update variables
+                game.step();
+                System.out.println(game.getGeneration());
+                lpane.repaint(); // Todo gen not updating
+            }
+        });
+        timer.start();
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                createAndShowGUI();
+            }
+        });
+    }
+
+
+
+}
